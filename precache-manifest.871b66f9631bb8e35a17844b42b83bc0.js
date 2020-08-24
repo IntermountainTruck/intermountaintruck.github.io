b2d106ed6d8f76af971369f47d9070f7b0cd61b0
Luke <luke@intermountaintruck.com>
2020-08-24 13:48:51 -0600

    Updates

---
+++ precache-manifest.871b66f9631bb8e35a17844b42b83bc0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4f0beba024ce8ecb47e07c42e5395f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cede27ad52df721ad054",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1fc0c0f8171ef72314e5",
+    "url": "/static/js/2.7fba9966.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.7fba9966.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cede27ad52df721ad054",
+    "url": "/static/js/main.8fad586e.chunk.js"
+  },
+  {
+    "revision": "620e7982d71b647ed2ed",
+    "url": "/static/js/runtime-main.4c34d486.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
